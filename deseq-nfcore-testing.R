2c0d270d78ca3fb0bc7500d8c1e882c2d258a207
Oliver-Lorenz-dev <85516197+Oliver-Lorenz-dev@users.noreply.github.com>
2021-10-04 19:52:08 +0100

    Update deseq-nfcore-testing.R

--- deseq-nfcore-testing.R
+++ deseq-nfcore-testing.R
@@ -1,4 +1,4 @@
-# DESeq2 differential gene expression analysis code
+# DESeq2 differential gene expression analysis code for nfcore-rnaseq results
 
 library(BiocManager)
 library(DESeq2)
@@ -9,20 +9,38 @@ library(tximport)
 
 run_table = read_csv('./demo_run_table.txt')
 
-# read in read count data using tximport
+# filter run table for values you want
+run_table_1 = filter(run_table, Condition == 'UNINDUCED')
+run_table_2 = filter(run_table, Condition == 'WT')
+
+run_table_final = full_join(run_table_1,run_table_2)
+
+# read in read count data
 count_data = read_tsv('./salmon.merged.gene_counts.tsv')
 
 count_data = data.frame(count_data)
 
+# drop unnecessary columns and set row names to gene ids
 count_data = count_data[,-1]
 rownames(count_data) = count_data[,1]
 
 count_data_final = subset(count_data, select = -c(1))
 
+# convert values to integer
 count_data_dds <- mutate_all(count_data_final, function(x) as.integer(as.character(x)))
 
-deseq_data <- DESeqDataSetFromMatrix(countData = count_data_dds,
-                              colData = run_table,
+# filter count data to match run table
+sample_list = run_table_final
+sample_list = data.frame(sample_list)
+rownames(sample_list) = sample_list[,1]
+
+final_samples = rownames(sample_list)
+count_data_filter = count_data_dds[final_samples]
+
+
+
+deseq_data <- DESeqDataSetFromMatrix(countData = count_data_filter,
+                              colData = run_table_final,
                               design= ~ Condition)
 
 # run DESeq2 analysis on data
