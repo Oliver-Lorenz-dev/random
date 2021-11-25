5dcc29787524b603b52a0f870a738249506d3c9a
Oliver-Lorenz-dev <85516197+Oliver-Lorenz-dev@users.noreply.github.com>
2021-11-25 10:11:10 +0000

    cleanup and fix edgeR matrix

--- edgeR_testing.R
+++ edgeR_testing.R
@@ -1,11 +1,7 @@
 library(edgeR)
-library(readr)
-library(dplyr)
-library(magrittr)
-library(tximport)
 library(statmod)
 
-x <- read.delim("salmon.merged.gene_counts.txt")
+x <- read.delim("rna-seq-chris-analysis/input_data/salmon.merged.gene_counts.txt")
 
 drops <- c("gene_id")
 x = x[ , !(names(x) %in% drops)]
@@ -29,10 +25,10 @@ y <- estimateDisp(y,design)
 logcpm <- cpm(y, log=TRUE)
 plotMDS(y)
 
-Cell <- factor(c(3,4,3,4))
+
 Toxin <- factor(c("Y","Y","N","N"))
-data.frame(Sample=colnames(y),Cell,Toxin)
-design <- model.matrix(~Cell+Toxin)
+data.frame(Sample=colnames(y),Toxin)
+design <- model.matrix(~Toxin)
 rownames(design) <- colnames(y)
 
 y <- estimateDisp(y, design, robust=TRUE)
@@ -54,33 +50,38 @@ pathway_df <- tibble::rownames_to_column(pathway_df, "Gene.symbol")
 colnames(pathway_df)[colnames(pathway_df) == 'PValue'] <- 'adj.P.val'
 
 library(pathfindR)
-RA_processed <- input_processing(input = pathway_df, # the input: in this case, differential expression results
+
+# process inputs
+RA_processed <- input_processing(input = pathway_df, # results from edgeR as input (in form of dataframe)
                                  p_val_threshold = 0.05, # p value threshold to filter significant genes
                                  pin_name_path  = "Biogrid", # the name of the PIN to use for active subnetwork search
                                  convert2alias = TRUE) # boolean indicating whether or not to convert missing symbols to alias symbols in the PIN
 
-#The available gene sets in pathfindR are “KEGG”, “Reactome”, “BioCarta”, “GO-All”, “GO-BP”, “GO-CC” and “GO-MF”. 
+# The available gene sets in pathfindR are “KEGG”, “Reactome”, “BioCarta”, “GO-All”, “GO-BP”, “GO-CC” and “GO-MF”. 
 biocarta_list <- fetch_gene_set(gene_sets = "BioCarta",
                                 min_gset_size = 10,
                                 max_gset_size = 300)
 biocarta_gsets <- biocarta_list[[1]]
 biocarta_descriptions <- biocarta_list[[2]]
 
-n_iter <- 10 ## number of iterations
-combined_res <- NULL ## to store the result of each iteration
+# number of iterations
+n_iter <- 10
+
+# to store the result of each iteration
+combined_res <- NULL
 
 for (i in 1:n_iter) {
   
-  ###### Active Subnetwork Search
-  snws_file <- paste0("active_snws_", i) # Name of output file
+  # Active Subnetwork Search
+  snws_file <- paste0("active_snws_", i) 
   active_snws <- active_snw_search(input_for_search = RA_processed, 
                                    pin_name_path = "Biogrid", 
                                    snws_file = snws_file,
-                                   score_quan_thr = 0.8, # you may tweak these arguments for optimal filtering of subnetworks
-                                   sig_gene_thr = 0.02, # you may tweak these arguments for optimal filtering of subnetworks
+                                   score_quan_thr = 0.8, # arg can be changed
+                                   sig_gene_thr = 0.02, # arg can be changed
                                    search_method = "GR")
   
-  ###### Enrichment Analyses
+  # Enrichment Analyses
   current_res <- enrichment_analyses(snws = active_snws,
                                      sig_genes_vec = RA_processed$GENE,
                                      pin_name_path = "Biogrid", 
@@ -88,23 +89,26 @@ for (i in 1:n_iter) {
                                      term_descriptions = biocarta_descriptions,
                                      adj_method = "bonferroni",
                                      enrichment_threshold = 0.05,
-                                     list_active_snw_genes = TRUE) # listing the non-input active snw genes in output
+                                     list_active_snw_genes = TRUE) 
   
-  ###### Combine results via `rbind`
+  # Combine results 
   combined_res <- rbind(combined_res, current_res)
 }
 
-###### Summarize Combined Enrichment Results
+# Summarize Combined Enrichment Results
 summarized_df <- summarize_enrichment_results(combined_res, 
                                               list_active_snw_genes = TRUE)
 
-###### Annotate Affected Genes Involved in Each Enriched Term
+# Annotate Affected Genes Involved in Each Enriched Term
 final_res <- annotate_term_genes(result_df = summarized_df, 
                                  input_processed = RA_processed, 
                                  genes_by_term = biocarta_gsets)
 
 visualize_terms(result_df = final_res, 
-                hsa_KEGG = FALSE, # boolean to indicate whether human KEGG gene sets were used for enrichment analysis or not
+                hsa_KEGG = FALSE, # true if using human KEGG gene set
                 pin_name_path = "Biogrid")
 
 enrichment_chart(final_res[1:10, ])
+
+## biocarta is the quickest pathway analysis to run as it is the
+## smallest gene set, this does take a while to run
